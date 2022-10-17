51b45c269edf49bf5b3c8a2e462ee799374dd673
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-10-17 23:46:12 +0200

    Add files via upload

--- tareemil.sh
+++ tareemil.sh
@@ -4,10 +4,10 @@ echo
 ## Command=wget https://raw.githubusercontent.com/tar1971/feedds/main/tareemil.sh -O - | /bin/sh
 ##
 echo
-wget -O /etc/opkg/user-feed.conf https://raw.githubusercontent.com/tar1971/feedds/main/emil-vip-feed.conf
+wget -O /etc/opkg/emil-vip-feed.conf https://raw.githubusercontent.com/tar1971/feedds/main/emil-vip-feed.conf
 wait
 opkg update
 reboot
 sleep 2;
 exit 0
-wweee
+uu
